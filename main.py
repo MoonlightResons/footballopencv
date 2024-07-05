445e67b72084dbefcb0462366c88c7c36ef90acc
Your Name <you@example.com>
2024-07-06 02:08:32 +0600

    new

--- main.py
+++ main.py
@@ -464,7 +464,7 @@ def result_goats(message):
         unique_id = str(uuid.uuid4())
         path = f'images/{club}.jpg'
         image_name = f'{club}_text_{unique_id}.jpg'
-        font_path = f'root/footbalopencv/fonts/{club}.TTF'
+        font_path = f'footbalopencv/fonts/{club}.TTF'
 
 
         create_image_for_goats(path, font_path, image_name, your_name, your_number)
